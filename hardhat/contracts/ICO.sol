87dff535cec164524b35a2fe2860852c5b48fcd8
Aakash <vermaaakash1000@gmail.com>
2023-01-10 15:08:49 +0530

    State variables and constructer created

---
+++ hardhat/contracts/ICO.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.3;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./ICryptoDevs.sol";
+
+contract cryptoDevToken is ERC20, Ownable {
+    uint256 public constant tokenPrice = 0.001 ether;
+    uint256 public constant tokensPerNft = 10 * 10 ** 18;
+    uint256 public constant maxTotalSupply = 10000 * 10 ** 18;
+    ICryptoDevs CryptoDevsNft;
+    //mapping to keep track of which tookenId have been claimed
+    mapping(uint256 => bool) public tokenIdsClaimed;
+
+    constructor(address _cryptoDevsContract) ERC20("Crypto Dev Token", "CD") {
+        CryptoDevsNft = ICryptoDevs(_cryptoDevsContract);
+    }
+    
+    function mint(uint256 amount) public payable{
+        require(msg.value >= tokenPrice*amount, "Ether sent is incorrect")
+    } 
+
+}
