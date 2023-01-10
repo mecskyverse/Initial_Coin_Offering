db909a90b0585fe9be0e0609ab3415d58de1d48d
Aakash <vermaaakash1000@gmail.com>
2023-01-10 15:22:22 +0530

    Mint function created

--- hardhat/contracts/ICO.sol
+++ hardhat/contracts/ICO.sol
@@ -16,4 +16,14 @@ contract cryptoDevToken is ERC20, Ownable {
     constructor(address _cryptoDevsContract) ERC20("Crypto Dev Token", "CD") {
         CryptoDevsNft = ICryptoDevs(_cryptoDevsContract);
     }
+
+    function mint(uint256 amount) public payable {
+        require(msg.value >= tokenPrice * amount, "Ether sent is incorrect");
+        uint256 amountWithDecimal = amount * 10 ** 18;
+        require(
+            totalSupply() + amountWithDecimal <= maxTotalSupply,
+            "You've exceeded the total supply available"
+        );
+        _mint(msg.sender, amountWithDecimal);
+    }
 }
